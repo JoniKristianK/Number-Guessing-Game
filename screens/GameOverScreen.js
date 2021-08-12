ff12b6f64e332786a87131462d41d3c25dfa785a
JoniKristianK <joni.karilainen@gmail.com>
2021-08-12 15:48:46 +0300

    Add: new game handler

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-const GameOverScreen = () => {
+const GameOverScreen = ({ roundsNumber, userNumber, onRestart }) => {
 	return (
 		<View style={styles.screen}>
 			<Text>Game Over!</Text>
+			<Text>Number of rounds: {roundsNumber}</Text>
+			<Text>Number was: {userNumber}</Text>
+			<Button title='NEW GAME' onPress={onRestart} />
 		</View>
 	);
 };
